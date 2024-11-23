3a84107615e9097938edea6fb2efaec96c841d3d
AZurisadai123 <america15.rf@gmail.com>
2024-11-23 10:11:15 -0600

    comun agregado

---
+++ SITICCommerce/BLL/ProductBLL/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductBLL
+{
+    internal class DAL
+    {
+        #region Variables y Properties
+
+        #endregion
+
+        #region Constructores
+
+        #endregion
+
+        #region Variables y Properties
+
+        #endregion
+    }
+}
--- SITICCommerce/Comun/VO/StaticDefinitions.cs
+++ SITICCommerce/Comun/VO/StaticDefinitions.cs
@@ -9,7 +9,7 @@ namespace VO
     public enum eStockStatus
     {
         IN_STOCK = 1,
-        OUT_STOCK,
+        OUT_OF_STOCK,
         LOW_STOCK
     }
 
@@ -18,5 +18,95 @@ namespace VO
         Inser = 1,
         Update = 2,
         Delete
+    }
+
+    public enum eErrorType
+    {
+        None = 0, //Ninguno
+        NotFound = 1, //No se encontro registro o cualquier datos
+        FormatError, //Error de conversion
+        Sql, //Error de sql
+        Validation, //Errores controlados
+        Connection, //Error de conexion
+        Timeout, //Error de timeout por bloqueos o interbloqueos
+        Unknown = 99 //Desconocido
+    }
+
+    //Representaciones
+    ///<sumary>
+    ///  Representa los diferentes esquemas quese manejan en este microservicio
+    ///</summary>
+
+    public class Schema
+    {
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string Products = "Products";
+
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string ProductConfigurations = "Product_Configurations";
+
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string Orders = "Orders";
+
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string OrderItems = "Order_Items";
+
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string Carts = "Carts";
+
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string CartItems = "Cart_Items";
+
+    }
+
+    //Representaciones
+    ///<sumary>
+    ///  Representa los procedimientos quese manejan en este microservicio
+    ///</summary>
+
+    public class Procedures
+    {
+        /// <summary>
+        /// Procedimiento para insertar
+        /// </summary>
+        public static readonly string Insert = "Insert";
+
+        /// <summary>
+        /// Procedimiento para actualizar
+        /// </summary>
+        public static readonly string Update = "Update";
+
+        /// <summary>
+        /// Procedimiento para Eliminar
+        /// </summary>
+        public static readonly string Delete = "Delete";
+
+        /// <summary>
+        /// Procedimiento para obtener un registro por identificador
+        /// </summary>
+        public static readonly string GetById = "GetById";
+
+        /// <summary>
+        /// Procedimiento para obtener todos los registros
+        /// </summary>
+        public static readonly string GetAll = "GetAll";
+
+        /// <summary>
+        /// Esquema Products
+        /// </summary>
+        public static readonly string CartItems = "Cart_Items";
+
     }
 }
